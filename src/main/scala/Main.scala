4f68c7680b29b3efb582607b1265fa1eb62affde
Oleksii Lisikh <alisiikh@gmail.com>
2024-05-13 00:29:32 +0200

    Use peel to read part of the stream and emit the rest of the stream to consume the rest

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,76 +14,88 @@ object App extends ZIOAppDefault:
         boundary = "boundary"
         numParts = 2
 
-        // read the stream till the separator, making a Part of headers and a stream body
-
+        // read the stream tokenize it into "events"
         bodyStream = ZStream.fromInputStream(body, 16) >>>
-          stringify >>>
+          ZPipeline.decodeStringWith(StandardCharsets.UTF_8) >>>
           ZPipeline.splitLines >>>
-          tokenize(boundary) >>>
-          ZPipeline.mapZIO { (part: Part) =>
-            part.content.runCollect.debug
+          tokenize(boundary)
+
+        _ <- bodyStream.broadcast(numParts + 1, 16).flatMap { streams =>
+          val jsonStream = streams(0) >>> takePart
+          val fileStream = streams(1) >>> skipParts(1) >>> takePart
+
+          val json = jsonStream.peel(headersSink).flatMap { case (headers, bodyStream) =>
+            Console.printLine(s"json-headers: $headers") *>
+              (bodyStream >>> collectBody).runCollect
           }
 
-        _ <- bodyStream.runDrain
+          val file = fileStream.peel(headersSink).flatMap { case (headers, bodyStream) =>
+            Console.printLine(s"file-headers: $headers") *>
+              (bodyStream >>> collectBody).runCollect
+          }
 
-        // streams <- bodyStream.broadcast(numParts + 1, 16)
+          val rest = streams(2).runDrain
 
-        // jsonStream = streams(0) >>> takePart >>> ZSink.collectAll[Event]
-        // fileStream = streams(1) >>> skipParts(1) >>> takePart >>> ZSink.collectAll[Event]
-
-        // drain the stream till the end to handle request
-        // restStream = streams(2) >>> ZSink.drain
-
-        // _ <- jsonStream.debug("JSON") <&> fileStream.debug("FILE") <&> restStream
+          (json.debug("json") <&> file.debug("file") <&> rest)
+        }
       } yield ()
     ).exitCode
   }
 
   def printEvents = ZPipeline.tap[Any, Throwable, Event](e => Console.printLine(e))
 
-  def stringify = ZPipeline.mapChunks[Byte, String](chunk => Chunk(new String(chunk.toArray, StandardCharsets.UTF_8)))
+  def tokenize(boundary: String): ZPipeline[Any, Throwable, String, Event] =
+    ZPipeline
+      .mapAccumZIO[Any, Throwable, String, Event, Event](Event.Start) {
+        case (Event.Start, line @ s"--$boundary") =>
+          val next = Event.Boundary(line)
+          ZIO.succeed((next, next))
 
-  def tokenize(boundary: String): ZPipeline[Any, Throwable, String, Part] =
-    ZPipeline.mapAccumZIO[Any, Throwable, String, (Event, Part), Part]((Event.Start, Part(Map.empty, ZStream.empty))) {
-      case ((Event.Start, part), line @ s"--$boundary") =>
-        val next = Event.Boundary(line)
-        val part = Part.empty
-        ZIO.succeed(((next, part), part))
+        case (Event.Start, line) =>
+          ZIO.fail(new IllegalArgumentException(s"unexpected content: '$line', expected boundary"))
 
-      case ((Event.Start, _), line) =>
-        ZIO.fail(new IllegalArgumentException(s"unexpected content: '$line', expected boundary"))
+        case (_: Event.Boundary, line) =>
+          val next = Event.Header.parse(line)
+          ZIO.succeed((next, next))
 
-      case ((_: Event.Boundary, part), line) =>
-        val header = Event.Header.parse(line)
-        val newPart = part.addHeader(header)
-        ZIO.succeed(((header, newPart), newPart))
+        case (event: Event.Header, line) => // line can be a header again or split line
+          val next =
+            if line.isEmpty then Event.Separator
+            else Event.Header.parse(line)
+          ZIO.succeed((next, next))
 
-      case ((event: Event.Header, part), line) => // line can be a header again or split line
-        if line.isEmpty then ZIO.succeed(((Event.Separator, part), part))
-        else
-          val header = Event.Header.parse(line)
-          val newPart = part.addHeader(header)
-          ZIO.succeed(((header, newPart), newPart))
+        case (Event.Separator, line) => // this must be content
+          val next = Event.Content(line)
+          ZIO.succeed((next, next))
 
-      case ((Event.Separator, part), line) => // this must be content
-        val next = Event.Content(line)
-        val newPart = part.addContent(line)
+        case (event: Event.Content, line) => // this is either content continues or boundary or end of multipart content
+          val next =
+            if line == s"--$boundary" then Event.Boundary(line)
+            else if line == s"--$boundary--" then Event.End
+            else Event.Content(line)
+          ZIO.succeed(next, next)
 
-        ZIO.succeed(((next, newPart), newPart))
+        case (Event.End, line) =>
+          ZIO.fail(IllegalArgumentException(s"no more content is expected, but got '$line'"))
+      }
 
-      case ((event: Event.Content, part), line) =>
-        // this is either content continues or boundary or end of multipart content
-        if line == s"--$boundary" then
-          val newPart = Part.empty
-          ZIO.succeed(((Event.Boundary(line), part), part))
-        else if line == s"--$boundary--" then ZIO.succeed(((Event.End, part), part))
-        else
-          val newPart = part.addContent(line)
-          ZIO.succeed(((Event.Content(line), newPart), newPart))
-
-      case ((Event.End, part), line) =>
-        ZIO.fail(IllegalArgumentException(s"no more content is expected, but got '$line'"))
+  def headersSink: ZSink[Any, Nothing, Event, Event, Map[String, String]] = ZSink
+    .collectAllWhile[Event] {
+      case _: Event.Header => true
+      case _               => false
     }
+    .map(
+      _.toList
+        .map {
+          case (header: Event.Header) => header.name -> header.value
+          case _                      => ("", "")
+        }
+        .toMap
+    )
+
+  def collectBody: ZPipeline[Any, Nothing, Event, String] = ZPipeline.collect { case event: Event.Content =>
+    event.value
+  }
 
   def skipParts(n: Int): ZPipeline[Any, Throwable, Event, Event] = {
     val pipe = ZPipeline.identity[Event]
@@ -112,53 +124,19 @@ object App extends ZIOAppDefault:
       }
   }
 
-// how?
-case class Part(headers: Map[String, String], content: ZStream[Any, Throwable, String]):
-  def addHeader(event: Event.Header): Part = this.copy(headers = headers ++ Map(event.name -> event.value))
-  def addContent(content: String): Part = this.copy(content = this.content ++ ZStream(content))
-
-object Part:
-  def empty: Part = Part(Map.empty, ZStream.empty)
-
 sealed trait Event
 
 object Event:
   case object Start extends Event
-
   case class Boundary(boundary: String) extends Event
+
   case class Header(name: String, value: String) extends Event
   object Header:
     def parse(line: String) =
       val name = line.takeWhile(_ != ':').trim.toLowerCase
       val value = line.dropWhile(_ != ':').drop(1).trim
       Header(name, value)
+
   case object Separator extends Event
   case class Content(value: String) extends Event
-
   case object End extends Event
-
-// sealed trait Event:
-// def parse(line: String): Event = this match
-//   case headers: Event.Headers =>
-//     headers.next(line)
-//   case content: Event.Content =>
-//     content.next(line)
-
-// object Event:
-// def start: Event = Headers(Map.empty)
-
-// case object StartPart extends Event
-
-// case class Headers(headers: Map[String, String]) extends Event:
-//   def next(line: String): Event =
-//     if (line == "") then Event.Content(headers, "")
-//     else
-//       val name = line.takeWhile(_ != ':').trim.toLowerCase
-//       val value = line.dropWhile(_ != ':').drop(1).trim
-//       Event.Headers(headers ++ Map(name -> value))
-//
-// case class Content(headers: Map[String, String], content: String) extends Event:
-//   def next(line: String): Event =
-//     Content(headers, content = content + line)
-
-// case object EndPart extends Event
